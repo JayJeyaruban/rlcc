54d63d75b48c6cbfafcb4f682205a91acc25df81
Jay Jeyaruban <jayjeyaruban@gmail.com>
2020-08-13 23:46:48 +0100

    Passed structure 4

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,22 @@
-use crate::tokenizer::{TokenParseResult, HAI};
+use crate::tokenizer::{TokenParseResult, HAI, KTHXBYE};
 
 const LATEST_LOLCODE_VERSION: f32 = 1.3;
 
 pub fn parse_tokens(tokens: &Vec<TokenParseResult>) {
   println!("{:?}", tokens);
 
-  for (i, token) in tokens.iter().enumerate() {
+  let filtered_tokens = tokens
+    .into_iter()
+    .filter(|token| {
+      !if let TokenParseResult::Token(t) = token {
+        t == " "
+      } else {
+        false
+      }
+    })
+    .collect::<Vec<&TokenParseResult>>();
+
+  for (i, token) in filtered_tokens.iter().enumerate() {
     if i == 0 {
       if let TokenParseResult::Token(t) = token {
         if t == HAI {
@@ -16,6 +27,13 @@ pub fn parse_tokens(tokens: &Vec<TokenParseResult>) {
       } else {
         panic!(format!("Unexpected token {:?}", token));
       }
+    } else if i as usize == filtered_tokens.len() - 1 {
+      if let TokenParseResult::Token(t) = token {
+        if t == KTHXBYE {
+          continue;
+        }
+      }
+      panic!(format!("Unexpected token {:?}", token));
     } else if i == 1 {
       if let TokenParseResult::Val(val) = token {
         if let Result::Ok(v) = val.parse::<f32>() {
