1583dfc1300a20a7fff3bbde4d883b582c902e5b
Jay Jeyaruban <jayjeyaruban@gmail.com>
2023-03-18 16:21:31 +0000

    refactor: rebrand to rlcc and reset

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
-#[macro_use]
-extern crate lazy_static;
-
 mod tokenizer;
 
 use std::env;
-use std::process::exit;
 use std::fs;
-
-use tokenizer::parse_tokens;
+use std::process::exit;
 
 fn main() {
-    let filename = env::args().nth(1);
-    if filename == None {
-        // Throw some error
-        exit(1);
-    }
+    // let filename = env::args().nth(2);
+    // if filename == None {
+    //     // Throw some error
+    //     exit(2);
+    // }
 
-    let file_contents = match filename {
-        None => exit(1),
-        Some(f) => fs::read_to_string(&f)
-    }.expect("Unable to read provided file.");
+    // let file_contents = match filename {
+    //     None => exit(2),
+    //     Some(f) => fs::read_to_string(&f),
+    // }
+    // .expect("Unable to read provided file.");
 
-    println!("{:?}",  parse_tokens(&file_contents));
+    // println!("{:?}", parse_tokens(&file_contents));
 }
