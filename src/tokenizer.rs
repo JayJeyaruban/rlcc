a321c604f221e68ae5e94d0284ffd5cf58df350a
Jay Jeyaruban <jayjeyaruban@gmail.com>
2023-03-19 18:13:32 +0000

    feat: pass hello world

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,46 +1,51 @@
-// use std::collections::HashSet;
+#[derive(Debug, PartialEq, Eq)]
+pub enum KeywordToken {
+    Hai,
+    KThxBye,
+}
 
-// use crate::tokenizer::TokenParseResult::{TOKEN, VAL};
+#[derive(Debug, PartialEq, Eq)]
+pub enum ParsedToken {
+    Keyword(KeywordToken),
+    Word(String),
+    Space,
+    NewLine,
+}
 
-// const HAI: &str = "HAI";
-// const KTHXBYE: &str = "KTHXBYE";
-// const SPACE: &str = " ";
-// const NL: &str = "\n";
+pub type TokenParseResult = Result<ParsedToken, String>;
 
-// lazy_static! {
-//     static ref TOKENS: HashSet<String> = vec![HAI, KTHXBYE, SPACE, NL].into_iter().map(|token| -> String {token.to_string()}).collect();
-// }
+pub fn parse_tokens(content_string: String) -> Vec<TokenParseResult> {
+    let mut parsed_tokens = Vec::new();
+    let mut buffer: Option<String> = None;
+    for c in content_string.chars() {
+        let tokens;
+        (buffer, tokens) = match buffer {
+            None => match c {
+                ' ' => (None, vec![Ok(ParsedToken::Space)]),
+                '\n' => (None, vec![Ok(ParsedToken::NewLine)]),
+                _ => (Some(String::from(c)), vec![]),
+            },
+            Some(mut buffer) => match c {
+                ' ' => (None, vec![parse_token(buffer), Ok(ParsedToken::Space)]),
+                '\n' => (None, vec![parse_token(buffer), Ok(ParsedToken::NewLine)]),
+                _ => {
+                    buffer.push(c);
+                    (Some(buffer), vec![])
+                }
+            },
+        };
 
+        tokens
+            .into_iter()
+            .for_each(|token| parsed_tokens.push(token));
+    }
+    parsed_tokens
+}
 
-// #[derive(Debug)]
-// pub enum TokenParseResult {
-//     TOKEN(String),
-//     VAL(String),
-// }
-
-// impl TokenParseResult {
-//     fn parse(s: &String) -> TokenParseResult {
-//         let s_str = s.as_str();
-//         if TOKENS.contains(s_str) { TOKEN(s.clone()) } else { VAL(s.clone()) }
-//     }
-// }
-
-
-// pub fn parse_tokens(content_string: &String) -> Vec<TokenParseResult> {
-//     let mut tokens = Vec::new();
-//     let mut current_word = String::new();
-//     for c in content_string.chars() {
-//         println!("{}", c);
-//         if c == ' ' || c == '\n' {
-//             if !current_word.is_empty() {
-//                 tokens.push(TokenParseResult::parse(&current_word));
-//                 current_word.clear();
-//                 tokens.push(TokenParseResult::parse(&c.to_string()));
-//             }
-//         } else {
-//             current_word.push(c);
-//         }
-//     }
-
-//     tokens
-// }
+fn parse_token(buffer: String) -> TokenParseResult {
+    match buffer.as_str() {
+        "HAI" => Ok(ParsedToken::Keyword(KeywordToken::Hai)),
+        "KTHXBYE" => Ok(ParsedToken::Keyword(KeywordToken::KThxBye)),
+        _ => Ok(ParsedToken::Word(buffer)),
+    }
+}
