34acd02963c9c948cac78005f03bee0ed01e9a19
Jay Jeyaruban <jayjeyaruban@gmail.com>
2023-03-23 01:16:31 +0000

    refactor: reduce dedup in parse_tokens

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -19,33 +19,51 @@ pub fn parse_tokens(content_string: String) -> Vec<TokenParseResult> {
     let mut buffer: Option<String> = None;
     for c in content_string.chars() {
         let tokens;
-        (buffer, tokens) = match buffer {
-            None => match c {
-                ' ' => (None, vec![Ok(ParsedToken::Space)]),
-                '\n' => (None, vec![Ok(ParsedToken::NewLine)]),
-                _ => (Some(String::from(c)), vec![]),
-            },
-            Some(mut buffer) => match c {
-                ' ' => (None, vec![parse_token(buffer), Ok(ParsedToken::Space)]),
-                '\n' => (None, vec![parse_token(buffer), Ok(ParsedToken::NewLine)]),
-                _ => {
-                    buffer.push(c);
-                    (Some(buffer), vec![])
-                }
-            },
-        };
+        (buffer, tokens) = process_char(c, buffer);
 
-        tokens
-            .into_iter()
-            .for_each(|token| parsed_tokens.push(token));
+        if let Some(tokens) = tokens {
+            tokens
+                .into_iter()
+                .for_each(|token| parsed_tokens.push(token));
+        }
     }
     parsed_tokens
 }
 
-fn parse_token(buffer: String) -> TokenParseResult {
-    match buffer.as_str() {
+fn process_char(
+    c: char,
+    buffer: Option<String>,
+) -> (Option<String>, Option<Vec<TokenParseResult>>) {
+    let parse_buffer_and_append = |buffer, token| {
+        let mut tokens = parse_word(buffer)
+            .map(|token| vec![token])
+            .unwrap_or_default();
+
+        tokens.push(Ok(token));
+        tokens
+    };
+
+    match c {
+        ' ' => (
+            None,
+            Some(parse_buffer_and_append(buffer, ParsedToken::Space)),
+        ),
+        '\n' => (
+            None,
+            Some(parse_buffer_and_append(buffer, ParsedToken::NewLine)),
+        ),
+        _ => {
+            let mut buffer = buffer.unwrap_or_default();
+            buffer.push(c);
+            (Some(buffer), None)
+        }
+    }
+}
+
+fn parse_word(buffer: Option<String>) -> Option<TokenParseResult> {
+    buffer.map(|buffer| match buffer.as_str() {
         "HAI" => Ok(ParsedToken::Keyword(KeywordToken::Hai)),
         "KTHXBYE" => Ok(ParsedToken::Keyword(KeywordToken::KThxBye)),
         _ => Ok(ParsedToken::Word(buffer)),
-    }
+    })
 }
