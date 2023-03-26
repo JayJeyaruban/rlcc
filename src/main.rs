1cdf9a27bc4353582b7e23319be02554c9118a6e
Jay Jeyaruban <jayjeyaruban@gmail.com>
2023-03-26 21:53:38 +0100

    test: add test for 1.1-1.3

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,13 @@ mod parser;
 mod tokenizer;
 
 use parser::process_tokens;
-use std::{fs, process::ExitCode};
+use std::{fs, path::Path, process::ExitCode};
 use tokenizer::{ParsedToken, TokenParseResult};
 
 use crate::tokenizer::parse_tokens;
 
-fn main() -> Result<ExitCode, Vec<String>> {
-    let filename = "tests/res/lci/test/1.3-Tests/1-Structure/1-EmptyMainBlock/test.lol";
-    // let filename = "tests/res/lci/test/1.3-Tests/1-Structure/2-MustBeginWithHAI/test.lol";
-    // let filename = "tests/res/lci/test/1.3-Tests/1-Structure/3-MustIncludeVersion/test.lol";
-
-    let file_contents = fs::read_to_string(&filename).unwrap();
-
+fn run<P: AsRef<Path>>(path: P) -> Result<(), Vec<String>> {
+    let file_contents = fs::read_to_string(path).expect("Unable to read provided file");
     let parse_results = parse_tokens(file_contents);
 
     let (tokens, errs) = split_errs(parse_results);
@@ -27,9 +22,17 @@ fn main() -> Result<ExitCode, Vec<String>> {
     if errs.len() > 0 {
         Err(errs)
     } else {
-        println!("Compilation successful");
-        Ok(ExitCode::SUCCESS)
+        Ok(())
     }
+}
+
+fn main() -> Result<ExitCode, Vec<String>> {
+    let filename = "tests/res/lci/test/1.3-Tests/1-Structure/1-EmptyMainBlock/test.lol";
+
+    run(filename).map(|_| {
+        println!("Compilation successful");
+        ExitCode::SUCCESS
+    })
 }
 
 fn split_errs(results: Vec<TokenParseResult>) -> (Vec<ParsedToken>, Vec<String>) {
@@ -43,4 +46,31 @@ fn split_errs(results: Vec<TokenParseResult>) -> (Vec<ParsedToken>, Vec<String>)
     }
 
     (tokens, errs)
+}
+
+#[cfg(test)]
+mod test {
+    use std::path::Path;
+
+    use test_generator::test_resources;
+
+    use crate::run;
+
+    #[test_resources("tests/res/lci/test/1.3-Tests/1-Structure/[1|2|3]-*")]
+    fn lci_test(resource: &str) {
+        let test_dir = Path::new(resource);
+
+        let contains_err_file = {
+            let mut err_file = test_dir.to_path_buf();
+            err_file.push("test.err");
+            err_file.is_file()
+        };
+
+        let mut input_file = test_dir.to_path_buf();
+        input_file.push("test.lol");
+
+        let result = run(input_file);
+
+        assert_eq!(contains_err_file, result.is_err())
+    }
 }
